7d2756e8a82dc0946c3b5f3bb6db1bfa2aba3323
Gaston <gaston@smartnet.com>
2021-02-03 23:31:02 -0300

    animation and changes css

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -8,7 +8,7 @@ const AboutMe = () => (
         <div className="container margin-proyects">
             <section className="skills-container">
                 <div className="about-container">
-                    <img className="img-main" width="100px" height="auto" src={ picture } alt="Foto de Gaston Camaño" />
+                    <img className="img-main-min" width="100px" height="auto" src={ picture } alt="Foto de Gaston Camaño" />
                     <div>
                         <p className="text">Hace ya un tiempo que me decidi por el desarrollo web con javascript, en el cual me siento muy cómodo . Soy una persona que dedica su tiempo a mejorar como profesional con buenas prácticas. Una de las cosas más importantes es resolver problemas y eso me resulta muy atractivo . </p>
                         <p className="text">Además de seguir en constante aprendizaje, tengo experiencia en trabajo en equipo y me desenvuelvo muy bien en ello. </p>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,8 @@
-import React from 'react' ;
+import React, { useState , useEffect} from 'react' ;
 import picture from '../images/foto-perfil-gaston.jpeg' ;
 import Nav from '../Components/Nav' ;
 import '../Styles/home.css' ;
+import TypeWriterEffect from 'react-typewriter-effect';
 
 const Home = () => (
     <>
@@ -9,8 +10,19 @@ const Home = () => (
         <main className="container align-content">
             <img className="img-main" width="200px" height="auto" src={ picture } alt="Foto de Gaston Camaño" />
             <section>
-                <h3 className="text">Hola mi nombre es Gastón.</h3>
-                <p className="text">Desarrollador Web</p>
+                {/* <h3 className="text animate-text">Hola mi nombre es Gastón.</h3> */}
+                <TypeWriterEffect
+                    textStyle={{ 
+                        fontFamily: 'monospace',
+                        color: 'wheat',
+                        paddingTop: '30px'
+                    }}
+                    startDelay={100}
+                    cursorColor="white"
+                    text="Hola mi nombre es Gastón."
+                    typeSpeed={100}
+                />
+                <p className="text" >Desarrollador Web</p>
             </section>
             <p className="gradient-text">by Gastón Camaño</p>
             <a className="gradient-text" style={ { paddingTop: '3px', backgroundColor: 'gray' } } target="blank" href="https://github.com/gaston3408/portfolio-gaston">
@@ -19,6 +31,6 @@ const Home = () => (
             </a>
         </main>
     </>
-) ;
+);
 
 export default Home ;
